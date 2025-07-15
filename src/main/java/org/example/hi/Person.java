c18b571ae77fdbbdac80568e6d6d8cf2cfe45803
Charnae Warrick <charnaexw@gmail.com>
2025-07-15 01:24:12 -0400

    Person Class

--- src/main/java/org/example/hi/Person.java
+++ src/main/java/org/example/hi/Person.java
@@ -1,5 +1,15 @@
 package org.example.hi;
 
 public class Person {
+    String name;
+    Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
-git
